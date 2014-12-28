8c2a115d12e47054af9ad1b6d19422757bb68c2c
edborden <borden.edward@gmail.com>
2014-12-28 11:53:45 -0500

    cleaning up  / test

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,6 @@
 Rails.application.routes.draw do
 
 	resources :sessions, only: [:create, :index, :destroy]
-	resources :locations, only: :create
-	resources :zones, only: :index
-	resources :notifications, only: :update
-
-	get 'hunts/join', to: 'hunts#join'
-	get 'hunts/found_target', to: 'hunts#found_target'
-	get 'hunts/expose', to: 'hunts#expose'
-	get 'hunts/unjoin', to: 'hunts#unjoin'
+	
 
 end
