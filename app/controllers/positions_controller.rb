0f27746ffc3ef202f1cbdd149eacef0eddfd4463
edborden <borden.edward@gmail.com>
2015-03-06 19:52:57 -0500

    headers v2

--- app/controllers/positions_controller.rb
+++ app/controllers/positions_controller.rb
@@ -1,9 +1,14 @@
 class PositionsController < ApplicationController
 
 	def create
-		page = Page.find params[:position][:page_id]
-		position = BlockBuilder.new(page,position_params).position
-		render json: position.reload, serializer: PositionSerializer, root:"position"
+		if params[:position][:page_id]
+			page = Page.find params[:position][:page_id]
+			position = BlockBuilder.new(page,position_params).position
+			position.reload
+		else #header position
+			position = Waterfall.new.position({params:position_params,block:MasterMold.new.default_block})
+		end
+		render json: position
 	end
 
 	def show #async:true in client block model
@@ -14,11 +19,11 @@ class PositionsController < ApplicationController
 	def update
 		position = Position.find params[:id]
 		Position.update position.id,position_params
-		render json: position.reload, serializer: PositionSerializer, root:"position"
+		render json: position.reload
 	end
 
 	def position_params
-		params.require(:position).permit :row,:col,:col_span,:row_span
+		params.require(:position).permit :row,:col,:col_span,:row_span,:user_id
 	end
 
 end
--- app/lib/add_headers.rb
+++ app/lib/add_headers.rb
@@ -3,7 +3,8 @@ module AddHeaders
 	def AddHeaders::to_this page
 		user = page.document.flow.folder.user
 		user.headers.each do |header| 
-			header = header.amoeba_dup 
+			header = header.amoeba_dup
+			header.user_id = nil
 			header.save
 			page.positions<<header
 		end
