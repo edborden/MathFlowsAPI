6471b427320d445f9dae0cdf8612d4ad33dc6e84
edborden <borden.edward@gmail.com>
2015-05-29 15:07:47 -0400

    copy block

--- app/controllers/blocks_controller.rb
+++ app/controllers/blocks_controller.rb
@@ -1,5 +1,19 @@
 class BlocksController < ResourceController
 
+	def create
+		if params[:block][:copy_from_id]
+			copy_from = Block.find params[:block][:copy_from_id]
+			@resource = copy_from.amoeba_dup
+			@resource.page_id = nil
+			@resource.col = nil
+			@resource.row = nil
+			@resource.save
+			render_resource
+		else
+			super
+		end
+	end
+
 	def resource_params
 		params.require(:block).permit :content,:question,:row,:col,:col_span,:row_span,:user_id,:page_id,:test_id
 	end
