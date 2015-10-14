1a46646f392ca03f591b29ec9562d7a1a9df7ab1
edborden <borden.edward@gmail.com>
2015-10-14 12:06:18 -0400

    blocks and tests metrics

--- app/controllers/metrics_controller.rb
+++ app/controllers/metrics_controller.rb
@@ -3,7 +3,9 @@ class MetricsController < ApplicationController
   def index
 
     metrics = {
-      users_count: Plan.not_guest.count
+      users_count: Plan.not_guest.count,
+      questions_count: Block.question.count,
+      tests_count: Test.count
     }
 
     render json: metrics
