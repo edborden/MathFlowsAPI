93b0902a02cf1c76aaa91a106557bc8dc13a665f
edborden <borden.edward@gmail.com>
2015-10-21 14:17:39 -0400

    GroupSerializer renders users with ColleagueSerializer

---
+++ app/serializers/colleague_serializer.rb
@@ -0,0 +1,8 @@
+class ColleagueSerializer < ApplicationSerializer
+  attributes :name,:pic,:email,:created_at,:tests_count
+  has_many :tests, embed: :ids, include: :true
+  has_many :folders, embed: :ids, include: :true
+  has_many :blocks, embed: :ids, include: :true
+  has_one :group
+
+end
--- app/serializers/group_serializer.rb
+++ app/serializers/group_serializer.rb
@@ -1,6 +1,6 @@
 class GroupSerializer < ApplicationSerializer
   attributes :name
-  has_many :users, embed: :ids, include: :true
+  has_many :users, embed: :ids, include: :true, serializer: ColleagueSerializer
 
   def users
     object.users - [current_user]
