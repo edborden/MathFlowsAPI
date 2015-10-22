d31aeecab763aa1923395e33576054431e9ed985
edborden <borden.edward@gmail.com>
2015-10-21 22:46:51 -0400

    Group metrics

--- app/controllers/metrics_controller.rb
+++ app/controllers/metrics_controller.rb
@@ -5,7 +5,8 @@ class MetricsController < ApplicationController
     metrics = {
       users_count: Plan.not_guest.count,
       questions_count: Block.question.count,
-      tests_count: Test.count
+      tests_count: Test.count,
+      groups_count: Group.with_multiple_users.count
     }
 
     render json: metrics
