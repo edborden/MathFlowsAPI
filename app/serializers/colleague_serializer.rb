07f655e5f0bf414114bf4ac9ce366c3b91272bf1
edborden <borden.edward@gmail.com>
2015-10-28 23:17:06 -0400

    Colleague shouldnt serialize blocks

--- app/serializers/colleague_serializer.rb
+++ app/serializers/colleague_serializer.rb
@@ -2,6 +2,5 @@ class ColleagueSerializer < ApplicationSerializer
   attributes :name,:pic,:email,:created_at,:tests_count
   has_many :tests, embed: :ids, include: :true
   has_many :folders, embed: :ids, include: :true
-  has_many :blocks, embed: :ids, include: :true
   has_one :group
 end
